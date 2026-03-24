acd763a27a776a8d9f6ee88398643b6e9a3b3edd
Vitor Caitano <caitanoovitor@gmail.com>
2026-03-24 15:52:13 -0300

    Inicio de um jogo de advinhação

---
+++ JogoDeAdvinhacao.ConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+Console.WriteLine("-----------------------------");
+Console.WriteLine("JOGO DE ADVINHAÇÃO");
+Console.WriteLine("-----------------------------");
+
+
+Console.WriteLine();
+Console.Write("DIGITE UM NÚEMRO: ");
+
+Console.ReadLine();
