6eb68e8c1f8b692cef325cd698b5e264b04b1c84
Vitor Caitano <caitanoovitor@gmail.com>
2026-03-24 15:54:50 -0300

    implementação das validações

--- JogoDeAdvinhacao.ConsoleApp/Program.cs
+++ JogoDeAdvinhacao.ConsoleApp/Program.cs
@@ -1,11 +1,40 @@
-﻿
+﻿using System.Security.Cryptography;
 
-Console.WriteLine("-----------------------------");
-Console.WriteLine("JOGO DE ADVINHAÇÃO");
-Console.WriteLine("-----------------------------");
+bool jodoDeveContinuar = true;
 
 
-Console.WriteLine();
-Console.Write("DIGITE UM NÚEMRO: ");
+while (jodoDeveContinuar == true)
+{
+    Console.WriteLine("-----------------------------");
+    Console.WriteLine("JOGO DE ADVINHAÇÃO");
+    Console.WriteLine("-----------------------------");
 
-Console.ReadLine();
+    int numeroAleatorio = RandomNumberGenerator.GetInt32(1, 21);
+
+    Console.WriteLine();
+    Console.Write("DIGITE UM NÚEMRO: ");
+    int numeroDigitado = Convert.ToInt32(Console.ReadLine());
+
+    // Console.WriteLine("O numero aleatorio foi: " + numeroAleatorio);
+
+    if (numeroDigitado == numeroAleatorio)
+    {
+        Console.WriteLine("Parabéns você acertou! O número aleatorio era exatamente " + numeroAleatorio);
+    }
+
+    else if (numeroDigitado < numeroAleatorio)
+    {
+        Console.WriteLine("O numero digitado foi menor que o numero secreto");
+    }
+
+    else
+    {
+        Console.WriteLine("O número digitado foi maior que o numero secreto");
+    }
+
+
+    Console.WriteLine();
+    Console.WriteLine("Digite ENTER para o jogo continuar...");
+    Console.ReadLine();
+}
+
